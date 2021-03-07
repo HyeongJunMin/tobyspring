ae03138e2e2696a0052abd081cb5dbbc0d633ded
HyeongJunMin <alsgudwns007@naver.com>
2021-03-07 22:56:34 +0900

    토비의스프링 6 AOP

---
+++ src/main/java/toby/common/proxy/TransactionHandler.java
@@ -0,0 +1,38 @@
+package toby.common.proxy;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TransactionHandler implements InvocationHandler {
+  // 부가기능을 제공할 타깃 객체. 어떤 타입의 객체든 적용할 수 있다.
+  @Setter private Object target;
+  // 트랜잭션을 제공하기 위한 트랜잭션 매니저
+  @Autowired private PlatformTransactionManager transactionManager;
+  // 트랜잭션을 적용할 메서드 이름 패턴
+  @Setter private String pattern;
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    if (method.getName().startsWith(pattern)) {
+      return invokeInTransaction(method, args);
+    } else {
+      return method.invoke(target, args);
+    }
+  }
+  private Object invokeInTransaction(Method method, Object[] args) throws Throwable {
+    TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+    try {
+      Object ret = method.invoke(target, args);
+      this.transactionManager.commit(status);
+      return ret;
+    } catch (InvocationTargetException e) {
+      this.transactionManager.rollback(status);
+      throw e.getTargetException();
+    }
+  }
+}
